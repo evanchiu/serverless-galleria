b66daf0883fd66982195fba336c165cb2530edcc
Evan Chiu <evan@evanchiu.com>
2017-12-31 21:48:57 -0800

    Refactored into promises

--- sepia/src/index.js
+++ sepia/src/index.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
-const s3 = new aws.S3();
-const path = require('path');
 const gm = require('gm').subClass({imageMagick: true});
+const path = require('path');
+const s3 = new aws.S3();
 
 const destBucket = process.env.DEST_BUCKET;
 
@@ -24,56 +24,77 @@ exports.handler = function main(event, context) {
 
   Promise.all(tasks)
     .then(() => { context.succeed(); })
-    .catch(() => { context.fail(); });
+    .catch((err) => { context.fail(err); });
 };
 
 function conversionPromise(record, destBucket) {
   return new Promise((resolve, reject) => {
     // The source bucket and source key are part of the event data
-    var srcBucket = record.s3.bucket.name;
-    var srcKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+    const srcBucket = record.s3.bucket.name;
+    const srcKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
 
     // Modify destKey if an alternate copy location is preferred
-    var destKey = srcKey;
-    var msg = 'sepia-ing ' + srcBucket + ':' + srcKey + ' to ' + destBucket + ':' + destKey;
+    const destKey = srcKey;
+    const conversion = 'converting ' + srcBucket + ':' + srcKey + ' to ' + destBucket + ':' + destKey;
 
-    console.log('Attempting: ' + msg);
-    // Get Object
+    console.log('Attempting: ' + conversion);
+
+    get(srcBucket, srcKey)
+      .then(original => sepia(original))
+      .then(modified => put(destBucket, destKey, modified))
+      .then(() => {
+        console.log('Success: ' + conversion);
+        return resolve('Success: ' + conversion);
+      })
+      .catch(error => {
+        console.error(error);
+        return reject(error);
+      });
+  });
+}
+
+function get(srcBucket, srcKey) {
+  return new Promise((resolve, reject) => {
     s3.getObject({
       Bucket: srcBucket,
-      Key: srcKey,
+      Key: srcKey
     }, (err, data) => {
       if (err) {
-        console.log('Error getting object:' + msg);
-        console.log(err, err.stack); // an error occurred
-        return reject('Error:' + msg);
+        console.error('Error getting object: ' + srcBucket + ':' + srcKey);
+        return reject(err);
+      } else {
+        resolve(data.Body);
       }
+    });
+  });
+}
 
-      // Convert to Sepia
-      gm(data.Body).sepia().toBuffer('JPG', (err, buf) => {
-        if (err) {
-          console.log('Error converting to sepia:' + msg);
-          console.log(err, err.stack); // an error occurred
-          return reject('Error:' + msg);
-        }
+function put(destBucket, destKey, data) {
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+      Bucket: destBucket,
+      Key: destKey,
+      Body: data
+    }, (err, data) => {
+      if (err) {
+        console.error('Error putting object: ' + destBucket + ':' + destKey);
+        return reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
 
-        // Put Object
-        s3.putObject({
-          Bucket: destBucket,
-          Key: destKey,
-          Body: buf
-        }, (err, data) => {
-          if (err) {
-            console.log('Error putting object:' + msg);
-            console.log(err, err.stack); // an error occurred
-            return reject('Error:' + msg);
-          }
-
-          // Success
-          console.log('Success: ' + msg);
-          return resolve('Success: ' + msg);
-        });
-      });
+function sepia(inBuffer) {
+  return new Promise((resolve, reject) => {
+    gm(inBuffer).sepia().toBuffer('JPG', (err, outBuffer) => {
+      if (err) {
+        console.error('Error applying sepia');
+        return reject(err);
+      } else {
+        resolve(outBuffer);
+      }
     });
   });
 }
