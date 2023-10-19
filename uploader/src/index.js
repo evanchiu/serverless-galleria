98b12fb8dac1f34c4a3f047103e2885082034ceb
Evan 🌟 Chiu <evan@evanchiu.com>
2023-10-18 23:27:26 -0400

    feat(uploader): modernized modules, runtime, AWS SDK, syntax, linting
    - Using EcmaScript Modules
    - Running on node18.x
    - Using AWS SDK for JavaScript v3
    - Using async/await
    - Using eslint

--- uploader/src/index.js
+++ uploader/src/index.js
@@ -1,133 +1,136 @@
-const aws = require('aws-sdk');
-const fs = require('fs');
-const mime = require('mime-types');
-const path = require('path');
-const s3 = new aws.S3();
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+const client = new S3Client();
+import { readFile } from "fs/promises";
+import { lookup } from "mime-types";
+import { join, resolve } from "path";
 
-const destBucket = process.env.DEST_BUCKET;
+const DEST_BUCKET = process.env.DEST_BUCKET;
 
-exports.handler = main;
-
-function main(event, context, lambdaCallback) {
-  // Fail on mising data
-  if (!destBucket) {
-    context.fail('Error: Environment variable DEST_BUCKET missing');
-    return;
+export async function handler(event) {
+  // Fail on mising config
+  if (!DEST_BUCKET) {
+    console.error("Error: Environment variable DEST_BUCKET missing");
+    return done(500, '{"message":"Internal Server Error"}');
   }
 
-  if (event.path.startsWith('/api/file/')) {
-    return fileRoute(event, context, lambdaCallback);
+  if (event.path.startsWith("/api/file/")) {
+    return fileRoute(event);
   } else {
-    return servePublic(event, context, lambdaCallback);
+    return servePublic(event);
   }
 }
 
-function fileRoute(event, context, lambdaCallback) {
-  if (event.httpMethod === 'POST') {
-    let key = event.path.replace('/api/file/', '');
+async function fileRoute(event) {
+  console.log("Serving fileRoute");
+  if (event.httpMethod === "POST") {
+    let key = event.path.replace("/api/file/", "");
 
     // Get the body data
     let body = event.body;
     if (event.isBase64Encoded) {
-      console.log('body is base-64 encoded');
-      body = Buffer.from(event.body, 'base64');
+      console.log("body is base-64 encoded");
+      body = Buffer.from(event.body, "base64");
     }
 
-    put(destBucket, key, body)
-      .then(() => {
-        message = 'Saved ' + destBucket + ':' + key;
-        console.log(message);
-        done(200, JSON.stringify({message:message}), 'application/json', lambdaCallback);
-      })
-      .catch((error) => {
-        console.error(error);
-        done(500, '{"message":"error saving"}', 'application/json', lambdaCallback);
-      });
+    try {
+      await put(DEST_BUCKET, key, body);
+
+      let message = "Saved " + DEST_BUCKET + ":" + key;
+      console.log(message);
+      return done(200, JSON.stringify({ message }));
+    } catch (error) {
+      console.error(error);
+      return done(500, '{"message":"error saving"}');
+    }
   } else {
-    return done(400, '{"message":"Invalid HTTP Method"}', 'application/json', lambdaCallback);
+    return done(400, '{"message":"Invalid HTTP Method"}');
   }
 }
 
-function servePublic(event, context, lambdaCallback) {
+async function servePublic(event) {
+  console.log(`Serving public for ${event.path}`);
   // Set urlPath
   let urlPath;
-  if (event.path === '/') {
-    return serveIndex(event, context, lambdaCallback);
+  if (event.path === "/") {
+    return serveIndex(event);
   } else {
     urlPath = event.path;
   }
 
   // Determine the file's path on lambda's filesystem
-  const publicPath = path.join(process.env.LAMBDA_TASK_ROOT, 'public');
-  const filePath = path.resolve(path.join(publicPath, urlPath));
-  const mimeType = mime.lookup(filePath);
+  const publicPath = join(process.env.LAMBDA_TASK_ROOT, "public");
+  const filePath = resolve(join(publicPath, urlPath));
+  const mimeType = lookup(filePath);
 
   // Make sure the user doesn't try to break out of the public directory
   if (!filePath.startsWith(publicPath)) {
-    console.log('forbidden', filePath, publicPath);
-    return done(403, '{"message":"Forbidden"}', 'application/json', lambdaCallback);
+    console.log("forbidden", filePath, publicPath);
+    return done(403, '{"message":"Forbidden"}', "application/json");
   }
 
   // Attempt to read the file, give a 404 on error
-  fs.readFile(filePath, function(err, data) {
-    if (err) {
-      return done(404, '{"message":"Not Found"}', 'application/json', lambdaCallback);
-    } else if (mimeType === 'image/png' ||
-        mimeType === 'image/jpeg' ||
-        mimeType === 'image/x-icon') {
+  try {
+    const data = await readFile(filePath);
+    if (
+      mimeType === "image/png" ||
+      mimeType === "image/jpeg" ||
+      mimeType === "image/x-icon"
+    ) {
       // Base 64 encode binary images
-      return done(200, data.toString('base64'), mimeType, lambdaCallback, true);
+      return done(200, data.toString("base64"), mimeType, true);
     } else {
-      return done(200, data.toString(), mimeType, lambdaCallback);
+      return done(200, data.toString(), mimeType);
     }
-  });
+  } catch (e) {
+    console.error("404", e);
+    return done(404, '{"message":"Not Found"}');
+  }
 }
 
 // Serve the index page
-function serveIndex(event, context, lambdaCallback) {
+async function serveIndex(event) {
+  console.log("Serving index");
   // Determine base path on whether the API Gateway stage is in the path or not
-  let base_path = '/';
-  if (event.requestContext.path.startsWith('/' + event.requestContext.stage)) {
-    base_path = '/' + event.requestContext.stage + '/';
+  let base_path = "/";
+  if (event.requestContext.path.startsWith("/" + event.requestContext.stage)) {
+    base_path = "/" + event.requestContext.stage + "/";
   }
 
-  let filePath = path.join(process.env.LAMBDA_TASK_ROOT, 'public/index.html');
+  let filePath = join(process.env.LAMBDA_TASK_ROOT, "public/index.html");
   // Read the file, fill in base_path and serve, or 404 on error
-  fs.readFile(filePath, function(err, data) {
-    if (err) {
-      return done(404, '{"message":"Not Found"}', 'application/json', lambdaCallback);
-    }
+  try {
+    const data = await readFile(filePath);
     let content = data.toString().replace(/{{base_path}}/g, base_path);
-    return done(200, content, 'text/html', lambdaCallback);
-  });
+    return done(200, content, "text/html");
+  } catch (error) {
+    console.error("404", error);
+    return done(404, '{"message":"Not Found"}');
+  }
 }
 
 // We're done with this lambda, return to the client with given parameters
-function done(statusCode, body, contentType, lambdaCallback, isBase64Encoded = false) {
-  lambdaCallback(null, {
+function done(
+  statusCode,
+  body,
+  contentType = "application/json",
+  isBase64Encoded = false
+) {
+  return {
     statusCode: statusCode,
     isBase64Encoded: isBase64Encoded,
     body: body,
     headers: {
-      'Content-Type': contentType
-    }
-  });
+      "Content-Type": contentType,
+    },
+  };
 }
 
 // Create a promise to put the data in the s3 bucket
-function put(destBucket, destKey, data) {
-  return new Promise((resolve, reject) => {
-    s3.putObject({
-      Bucket: destBucket,
-      Key: destKey,
-      Body: data
-    }, (err, data) => {
-      if (err) {
-        console.error('Error putting object: ' + destBucket + ':' + destKey);
-        return reject(err);
-      } else {
-        resolve(data);
-      }
-    });
+async function put(destBucket, destKey, data) {
+  const command = new PutObjectCommand({
+    Bucket: destBucket,
+    Key: destKey,
+    Body: data,
   });
+  return client.send(command);
 }
