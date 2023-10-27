4f374e47c24cc1d9b8042120605394b40f522d15
Evan 🌟 Chiu <evan@evanchiu.com>
2023-10-26 23:52:25 -0400

    feat(util): moved more functionality to shared util

--- blur/src/index.js
+++ blur/src/index.js
@@ -1,36 +1,17 @@
 import Jimp from "jimp";
-import { transformObject } from "serverless-galleria-util";
+import { handle } from "serverless-galleria-util";
 
+/** Handle the event from s3 */
 export async function handler(event) {
-  // Fail on mising config
-  const destBucket = process.env.DEST_BUCKET;
   const blurRadius = parseInt(process.env.BLUR_RADIUS);
-  if (!destBucket) {
-    throw new Error("Error: Environment variable DEST_BUCKET missing");
-  } else if (typeof blurRadius !== "number") {
+  if (typeof blurRadius !== "number") {
     throw new Error("Error: Environment variable BLUR_RADIUS missing");
-  } else if (event.Records === null) {
-    throw new Error("Error: Event has no records.");
   }
-
-  const transformBuffer = getTransformBuffer(blurRadius);
-
-  // Transform all records
-  await Promise.all(
-    event.Records.map(async (record) => {
-      // The source bucket and source key are part of the event data
-      const srcBucket = record.s3.bucket.name;
-      const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
-      await transformObject(srcBucket, key, destBucket, key, transformBuffer);
-      console.log(`blurred (radius ${blurRadius}): ${srcBucket}:${key} to ${destBucket}:${key}`);
-    })
-  );
-}
-
-function getTransformBuffer(blurRadius) {
-  return async (inBuffer) => {
+  console.log(`Transforming with blur radius (${blurRadius})`);
+  
+  await handle(event, async (inBuffer) => {
     const image = await Jimp.read(inBuffer);
     image.blur(blurRadius);
     return image.getBufferAsync(Jimp.MIME_JPEG);
-  };
+  });
 }
--- serverless-galleria-util/index.js
+++ serverless-galleria-util/index.js
@@ -5,11 +5,28 @@ import {
 } from "@aws-sdk/client-s3";
 const s3 = new S3Client();
 
-/** Read a file from one bucket, apply the transform, and store in another bucket */
-export async function transformObject(srcBucket, srcKey, destBucket, destKey, transformBuffer) {
-  const original = await get(srcBucket, srcKey);
-  const modified = await transformBuffer(original);
-  await put(destBucket, destKey, modified);
+/** Handle the s3 event by loading the s3 record files from s3, running the given transformer on each, saving to the dest bucket */
+export async function handle(event, transformer) {
+  // Fail on mising config
+  const destBucket = process.env.DEST_BUCKET;
+  if (!destBucket) {
+    throw new Error("Error: Environment variable DEST_BUCKET missing");
+  }
+
+  // Transform all records
+  await Promise.all(
+    event.Records.map(async (record) => {
+      const srcBucket = record.s3.bucket.name;
+      const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+      console.log(
+        `Transforming ${srcBucket}:${key} to ${destBucket}:${key}...`
+      );
+      const original = await get(srcBucket, key);
+      const modified = await transformer(original);
+      await put(destBucket, key, modified);
+      console.log(`Transformed ${srcBucket}:${key} to ${destBucket}:${key}`);
+    })
+  );
 }
 
 /** Get file from S3 as a Buffer */
