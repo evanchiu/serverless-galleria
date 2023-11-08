4d81a752f0c7fd5ba71193bf16706505f9060c56
Evan 🌟 Chiu <evan@evanchiu.com>
2023-11-07 22:27:03 -0500

    feat(rotate): modernize

--- rotate/src/index.js
+++ rotate/src/index.js
@@ -1,99 +1,21 @@
-const aws = require('aws-sdk');
-const jimp = require('jimp');
-const s3 = new aws.S3();
+import Jimp from "jimp";
+import { handle } from "serverless-galleria-util";
 
-const destBucket = process.env.DEST_BUCKET;
-const rotateDegrees = parseInt(process.env.ROTATE_DEGREES);
-const backgroundColor = process.env.BACKGROUND_COLOR;
-
-exports.handler = function main(event, context) {
-  // Fail on mising data
-  if (!destBucket || !backgroundColor || !rotateDegrees) {
-    context.fail('Error: Environment variable missing');
-    return;
+/** Handle the event from s3 */
+export async function handler(event) {
+  const rotateDegrees = parseInt(process.env.ROTATE_DEGREES);
+  const backgroundColor = process.env.BACKGROUND_COLOR;
+  if (typeof rotateDegrees !== "number") {
+    throw new Error("Error: Environment variable ROTATE_DEGREES missing");
   }
-  if (event.Records === null) {
-    context.fail('Error: Event has no records.');
-    return;
+  if (!backgroundColor.match(/^#[0-9a-fA-F]{6}$/)) {
+    throw new Error("Error: Expected BACKGROUND_COLOR hex format, e.g. \"#00CCFF\"")
   }
+  console.log(`Rotating ${JSON.stringify(rotateDegrees, backgroundColor)}`);
 
-  // Make a task for each record
-  let tasks = [];
-  for (let i = 0; i < event.Records.length; i++) {
-    tasks.push(conversionPromise(event.Records[i], destBucket));
-  }
-
-  Promise.all(tasks)
-    .then(() => { context.succeed(); })
-    .catch((err) => {
-      console.error('failed');
-      console.error(JSON.stringify(event));
-      console.error(JSON.stringify(context));
-      context.fail(err);
-    });
-};
-
-function conversionPromise(record, destBucket) {
-  return new Promise((resolve, reject) => {
-    // The source bucket and source key are part of the event data
-    const srcBucket = record.s3.bucket.name;
-    const srcKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
-
-    // Modify destKey if an alternate copy location is preferred
-    const destKey = srcKey;
-    const conversion = 'rotating (' + rotateDegrees + '° ' + backgroundColor + '): ' + srcBucket + ':' + srcKey + ' to ' + destBucket + ':' + destKey;
-
-    console.log('Attempting: ' + conversion);
-
-    get(srcBucket, srcKey)
-      .then(original => rotate(original))
-      .then(modified => put(destBucket, destKey, modified))
-      .then(() => {
-        console.log('Success: ' + conversion);
-        return resolve('Success: ' + conversion);
-      })
-      .catch(error => {
-        console.error(error);
-        return reject(error);
-      });
+  await handle(event, async (inBuffer) => {
+    const image = await Jimp.read(inBuffer);
+    image.background(Jimp.cssColorToHex(backgroundColor)).rotate(rotateDegrees);
+    return image.getBufferAsync(Jimp.MIME_JPEG);
   });
-}
-
-function get(srcBucket, srcKey) {
-  return new Promise((resolve, reject) => {
-    s3.getObject({
-      Bucket: srcBucket,
-      Key: srcKey
-    }, (err, data) => {
-      if (err) {
-        console.error('Error getting object: ' + srcBucket + ':' + srcKey);
-        return reject(err);
-      } else {
-        resolve(data.Body);
-      }
-    });
-  });
-}
-
-function put(destBucket, destKey, data) {
-  return new Promise((resolve, reject) => {
-    s3.putObject({
-      Bucket: destBucket,
-      Key: destKey,
-      Body: data
-    }, (err, data) => {
-      if (err) {
-        console.error('Error putting object: ' + destBucket + ':' + destKey);
-        return reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-}
-
-async function rotate(inBuffer) {
-  const image = await jimp.read(inBuffer);
-  image.background(jimp.cssColorToHex(backgroundColor)).rotate(rotateDegrees);
-  return image.getBufferAsync(jimp.MIME_JPEG);
 }
